2a7efc179a559f25654af8655b23e4f8b6a4d7da
Edgar Arroyo <Edgar@iMac-de-Edgar.local>
2017-03-01 00:47:03 -0600

    Goto recipies is finished

--- public/js/angularmodules/welcome_module.js
+++ public/js/angularmodules/welcome_module.js
@@ -2,4 +2,10 @@ var wapp = angular.module('wapp',[]);
 
 wapp.controller('MainController', ['$scope', '$http', function($scope, $http){
   console.log(1);
+
+
+    $('#gotorecetas').on('click', function (e) {
+        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    });
+
 }]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,14 @@
 <body ng-app="wapp" ng-controller="MainController">
   <div class="container-fluid">
     <div class="row">
+      <div id="gotorecetasDiv">
+        <a href="#" id="gotorecetas"><img src="assets/down-arrow.png" class="img-responsive img-gotorecetas" alt=""></a>
+        <p class="gotorecetas-text">Ver las últimas recetas</p>
+
+      </div>
+
+    </div>
+    <div class="row">
       <div class="col-md-6">
         <img src="assets/mexican-woman.png" class="logo-img img-responsive"alt="">
         <h1 class="landing-title">El Maya indomable</h1>
