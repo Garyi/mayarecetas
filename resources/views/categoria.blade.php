c925c0c85b6015fe2597576748ef5dfa705f86bf
Edgar Arroyo <Edgar@iMac-de-Edgar.local>
2017-03-04 21:35:22 -0600

    hotfix in categorias

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -120,18 +120,25 @@
         <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
       </div>
     </div>
-  </div>
+    </div>
   <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-      <img src="assets/carousel/carousel3.jpg" alt="...">
+      <img src="assets/carousel/carousel2.jpg" alt="...">
       <div class="caption">
-        <h3>Café muy mexicano, creo</h3>
+        <h3>Frijoles muy mexicanos, creo</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
               occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit an
+              im id est laborum.</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+    </div>
+
   </div>
+</div>
 </body>
 </html>
