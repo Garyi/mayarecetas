25d864bb0883075023de2add2127f9b52ebea95b
Garyi <hi.ed@hotmail.com>
2017-05-03 11:38:16 -0500

    new database

--- dbase/mayadb.sql
+++ dbase/mayadb.sql
@@ -2,10 +2,10 @@
 -- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 28, 2017 at 11:31 
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Servidor: localhost
+-- Tiempo de generación: 03-05-2017 a las 18:36:51
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mayadb`
+-- Base de datos: `mayadb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `baules`
+-- Estructura de tabla para la tabla `baules`
 --
 
 CREATE TABLE `baules` (
@@ -33,7 +33,7 @@ CREATE TABLE `baules` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `baules`
+-- Volcado de datos para la tabla `baules`
 --
 
 INSERT INTO `baules` (`id`, `user_id`, `receta_id`) VALUES
@@ -43,7 +43,7 @@ INSERT INTO `baules` (`id`, `user_id`, `receta_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lugares`
+-- Estructura de tabla para la tabla `lugares`
 --
 
 CREATE TABLE `lugares` (
@@ -53,18 +53,20 @@ CREATE TABLE `lugares` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `lugares`
+-- Volcado de datos para la tabla `lugares`
 --
 
 INSERT INTO `lugares` (`id`, `nombre`, `status`) VALUES
 (16, 'Yucatán', 1),
 (17, 'Jalisco', 1),
-(18, 'Tlaxcala de Xicohténcatl', 1);
+(18, 'Tlaxcala de Xicohténcatl', 1),
+(19, 'Guanajuato', 1),
+(20, 'Michoacan', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `perfiles`
+-- Estructura de tabla para la tabla `perfiles`
 --
 
 CREATE TABLE `perfiles` (
@@ -73,7 +75,7 @@ CREATE TABLE `perfiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `perfiles`
+-- Volcado de datos para la tabla `perfiles`
 --
 
 INSERT INTO `perfiles` (`id`, `nombre`) VALUES
@@ -84,7 +86,7 @@ INSERT INTO `perfiles` (`id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recetas`
+-- Estructura de tabla para la tabla `recetas`
 --
 
 CREATE TABLE `recetas` (
@@ -97,7 +99,7 @@ CREATE TABLE `recetas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `recetas`
+-- Volcado de datos para la tabla `recetas`
 --
 
 INSERT INTO `recetas` (`id`, `titulo`, `lugar`, `descripcion`, `aprobacion`, `portada`) VALUES
@@ -111,12 +113,17 @@ INSERT INTO `recetas` (`id`, `titulo`, `lugar`, `descripcion`, `aprobacion`, `po
 (9, 'Poc Chuc', 16, '....\r\n\r\nIngredientes\r\n4 raciones\r\nCarne de cerdo (en bisteces)\r\nIngredientes para marinar el cerdo\r\n1 taza naranja agria\r\npasta de achiote\r\n4 ctas sal\r\n2 ctas orégano\r\n2 ctas tomillo\r\n2 ctas pimienta molida\r\nIngredientes para la cebolla morada\r\n10 cebollas rojas medianas cortadas en cuadros\r\n8 tazas agua\r\nVinagre\r\n1 atado cilantro picadito\r\n1/2 chile habanero asado y picadito\r\nSal y pimienta\r\n\r\n\r\nPasos\r\n75 minutos\r\nCómo preparar la carne: \r\nPonemos a marinar la carne durante 3 horas en nuestra mezcla (Ingredientes que hemos elegido para marinar, ver en el apartado de ingredientes), previamente preparada .\r\nCasi a la hora de servir, es cunado asaremos a la parrilla los bistecs de carne.\r\nCómo preparar las cebollas: \r\nEchamos agua a calentar en una cacerola y añadimos las cebollas cortadas en cuadros durante 1 minuto.\r\nRetiramos las cebollas del fuego, las escurrimos y les agregamos el vinagre o naranja agria y el cilantro.\r\nAñadimos la sal y la pimienta al gusto.\r\nEmplatar: \r\nServimos la carne acompañada de la cebolla, 1/4 de naranja agria y una salsa de chiltomate.\r\nAcompañamos como guarnición un platito de frijoles colados y tortillas.\r\nSe puede preparar también una salsa picante tamulada.', 1, 'files/1493324930poc.jpg'),
 (10, 'Papadzules', 16, '......\r\nIngredientes\r\n4 raciones\r\n1/2 kg pepita de calabaza para papadzul\r\n16 huevos cocidos y picados en cuadros\r\nEpazote\r\n1/4 kg tortilla\r\n8 tomates\r\n\r\n\r\nPasos\r\n30 minutos\r\nCocemos los huevos en agua hasta que veamos que la cáscara se raja. Los pelamos.\r\nEn una olla ponemos a hervir agua con el epazote. Una vez que el epazote haya soltado su punto, la partamos del fuego y aguardamos a que esté fría.\r\nLe echamos la pepita molida y movemos la mezcla poco a poco hasta que observemos que tiene una consistencia cremosa.\r\nPara elaborar la salsa de Papadzul\r\nHervimos en agua los tomates, se quita la cáscara y se licuan.\r\nEn una sartén echamos aceite, el tomate ya licuado, un poco de cebolla picada, sal y, si lo desea muy picante, chile habanero asado.\r\nLo freímos todo hasta que obtengamos una salsa espesita.\r\nEmplatado del Papdzul:\r\nColocar en cada plato 4 \"tacos\" de huevo con la tortilla. Se bañan con la pepita y se adornan con la salsa de tomate.\r\nEste antojito se come caliente y si se quiere resaltar el picante con un chile habanero asado adicional.', 1, 'files/1493324996papadzules.jpg'),
 (11, 'Tikin Xic', 16, '....\r\nIngredientes\r\n6 raciones\r\n110 gr Axiote\r\n1/4 de Taza Cebolla picada\r\n3 Clavos\r\n4 Dientes Ajo limpios\r\n3 Pimientas Gordas\r\n1 Cda Pimineta Negra entera\r\n1/2 Taza Jugo de Naranja\r\n1/2 Taza Jugo de Limón\r\n1/4 Taza Aceite de Oliva\r\n6 Filetes de Pescado\r\nSal y Pimienta\r\n\r\n\r\nPasos\r\n30 minutos\r\nSalpimentar los filetes de Pescado y reservar.\r\nLicuar el resto de los ingredientes y marinar los pescados en la salsa por espacio de una hora.\r\nUna vez listos llevar a una sartén o a la parrilla.', 1, 'files/1493325048Tikin-Xic.jpg'),
-(12, 'Longaniza de Valladolid', 16, '....\r\nIngredientes\r\n4 raciones\r\n1 1/2 longaniza fresca\r\nSal\r\npimienta\r\n1 taza leche evaporada\r\nCilantro\r\najo\r\naceite de oliva\r\nRúcula\r\n1 tomate\r\nvinagre balsámico\r\n\r\n\r\nPasos\r\n20 minutos\r\nHervir 10 minutos la longaniza, mientras preparar la rúcula y tomates con una pizca de sal, aceite de oliva y vinagre balsámico.\r\nSofreír en el aceite el cilantro, ajo con una pizca de sal, agregar la leche evaporada cuando hierva agregar la longaniza picadita.\r\nA disfrutar!!', 1, 'files/1493325094valla.jpg');
+(12, 'Longaniza de Valladolid', 16, '....\r\nIngredientes\r\n4 raciones\r\n1 1/2 longaniza fresca\r\nSal\r\npimienta\r\n1 taza leche evaporada\r\nCilantro\r\najo\r\naceite de oliva\r\nRúcula\r\n1 tomate\r\nvinagre balsámico\r\n\r\n\r\nPasos\r\n20 minutos\r\nHervir 10 minutos la longaniza, mientras preparar la rúcula y tomates con una pizca de sal, aceite de oliva y vinagre balsámico.\r\nSofreír en el aceite el cilantro, ajo con una pizca de sal, agregar la leche evaporada cuando hierva agregar la longaniza picadita.\r\nA disfrutar!!', 1, 'files/1493325094valla.jpg'),
+(13, 'Albóndigas con chicharrón', 19, '....\r\ningredientes:\r\n3/4 kilo carne de res molida\r\n150 g chicharrón delgado\r\n3 tazas de agua\r\n4 cucharadas de cilantro finamente picado\r\n2 1/2 cucharadas de consomé de pollo en polvo\r\n2 cucharadas de aceite\r\n2 cucharadas de cebolla finamente picada\r\n4 chiles jalapeños, en rajas\r\n4 jitomates grandes, picados\r\n2 dientes de ajo, finamente picados\r\n2 huevos crudos\r\npimienta\r\nPara preparar albóndigas con chicharrón hay que triturar muy bien el chicharrón y mezclarlo con la carne, los huevos, el cilantro, ajo y cebolla, sazonando con la pimienta y la mitad del consomé en polvo. Formar las albóndigas con las manos húmedas y dejarlas reposar quince minutos. Freír juntos los jitomates y los chiles, en aceite caliente. Agregarles el agua y el resto del consomé. Al hervir, incorporar las albóndigas. Dejar cocer durante veinte minutos más, a fuego lento. La receta alcanza para 8 raciones.', 1, 'files/1493828839albondigas.jpg'),
+(14, 'Asado en vino', 19, '1 kilo lomo de cerdo, en trozos\r\n1/4 kilo papas cocidas\r\n150 g ciruela\r\n1/2 taza de crema\r\n1 taza de leche\r\n1 taza de vinagre\r\n1/2 botella de vino blanco\r\n1 cucharada de azúcar\r\n1 cucharadita de mostaza\r\n1 cucharadita de tomillo\r\n6 soletas\r\n4 dientes de ajo\r\n3 betabeles cocidos\r\n3 jitomates asados (molidos con una cebolla y colados)\r\n2 hojas de laurel\r\nchiles jalapeños\r\npimienta\r\nsal\r\nPara preparar asado en vino blanco hay que freír el lomo. Agregarle un poco de agua, vinagre y vino blanco, las ciruelas deshuesadas, laurel, ajos, tomillo, azúcar, sal y dejarlo hervir a fuego suave. Ya que esté blanda la carne y se haya resecado un poco el agua, sacar los ajos y las hojas de laurel; agregar el jitomate colado, desmoronar las soletas e incorporarlas para formar una salsa, no muy espesa. Dejar hervir otro rato y retirar de la lumbre. Servir con la siguiente ensalada: papas y betabeles (cocidos y picados), sal, pimienta, chiles jalapeños (picados), un poco del vinagre de los chiles, crema, leche y una cucharadita de mostaza. La receta alcanza para 8 raciones.', 1, 'files/1493828888asadobino.jpg'),
+(15, 'SOPA TARASCA', 20, '...\r\nINGREDIENTES\r\n6 cucharadas de aceite de maíz\r\n1 cebolla grande picada finamente\r\n2 dientes de ajo grandes picados finamente\r\n1 lata de 1 kilo de puré de jitomate\r\n2 litros de agua\r\n4 hojas de laurel\r\n2 ramitas de tomillo\r\n2 ramitas de mejorana\r\n4 cucharadas de caldo de pollo en polvo\r\n1 cucharadita de pimienta negra recién molida\r\n6 chiles anchos despepitados, cortados en tiras y fritos (en Michoacán se les\r\nlama \"Pasilla\")\r\n10 tortillas cortadas en tiritas y fritas\r\n350 gramos de queso fresco cortado en tiras\r\n¼ de litro de crema espesa\r\nEpazote picado al gusto\r\nPREPARACIÓN\r\nEn el aceite caliente se acitronan la cebolla y el ajo, se añade el puré de jitomate y se fríe hasta que esté \"chinito\", se agrega entonces el agua, el caldo, las hierbas de olor y la pimienta. Se deja hervir 10 minutos. Se toma un poco de caldillo y con él se licúan dos chiles fritos y una tortilla frita, se vierten en la sopa hirviendo y se deja hervir a fuego lento 5 minutos más.\r\nPRESENTACIÓN\r\nEn platos soperos individuales con la tortilla frita, las tiritas de chile frito, las tiritas de queso, una cucharada de crema y epazote picado al gusto. Sirve y disfruta de tu Sopa Tarasca, platillo de Michoacán.\r\n\r\n', 1, 'files/1493829130tarasca.jpg'),
+(16, 'Morisqueta', 20, '....\r\nINGREDIENTES\r\n1 kilo carne de cerdo\r\n1/2 kilo jitomate\r\n2 tazas de agua con sal\r\n1 taza de arroz entero, limpio\r\n6 chiles serranos, verdes\r\n2 cebollas\r\n1 ajo\r\nPara preparar morisqueta hay que poner en un recipiente el agua con sal; cuando hierva, agregar el arroz y bajar la flama hasta que el grano se cueza. Poner la carne de cerdo en una sartén con agua suficiente para que la cubra y sal, hasta que se cueza y se consuma el agua. Se fríe. Aparte, moler los jitomates con las cebollas, ajo, chiles y agregar a la carne (con este guiso se acompaña la morisqueta). Se sirve muy caliente. La receta alcanza para 6 raciones.\r\n', 1, 'files/1493829159morisqueta.jpg'),
+(17, 'Aporreadillo Michoacano', 20, 'INGREDIENTES\r\n1/2 kilo de cecina\r\n3 huevos\r\n100 gramos de chile pasilla\r\n1/2 kilo de tomate verde\r\n\r\nSe escoge la cecina poco grasosa, se dora a fuego lento hasta que quiebre, se machaca y se pone a freir, se le agrega el chile molido, junco con el tomate cocido y se deja hervir bastante. Cuando está bien seca se agregan huevos cortados y se revuelven rápidamente.\r\nY para acompañar este platillo un buen mezcal Los Danzantes Reposado que se distingue por su escencia al dulce de frutas cocidas y especias. Para el comienzo y final de del disfrute de este platillo', 1, 'files/1493829186aporreadillo.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recetas_calificacion`
+-- Estructura de tabla para la tabla `recetas_calificacion`
 --
 
 CREATE TABLE `recetas_calificacion` (
@@ -127,19 +134,20 @@ CREATE TABLE `recetas_calificacion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `recetas_calificacion`
+-- Volcado de datos para la tabla `recetas_calificacion`
 --
 
 INSERT INTO `recetas_calificacion` (`id`, `receta_id`, `usuario_id`, `calificacion`) VALUES
 (18, 2, 5, 5),
 (19, 6, 5, 2),
 (20, 7, 5, 3),
-(21, 9, 5, 4);
+(21, 9, 5, 4),
+(22, 2, 3, 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -151,7 +159,7 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `username`, `password`, `perfil_id`, `email`) VALUES
@@ -159,76 +167,76 @@ INSERT INTO `usuarios` (`id`, `username`, `password`, `perfil_id`, `email`) VALU
 (5, 'webmaster', '50a9c7dbf0fa09e8969978317dca12e8', 2, 'xbox_live@live.com');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `baules`
+-- Indices de la tabla `baules`
 --
 ALTER TABLE `baules`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `lugares`
+-- Indices de la tabla `lugares`
 --
 ALTER TABLE `lugares`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `perfiles`
+-- Indices de la tabla `perfiles`
 --
 ALTER TABLE `perfiles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `recetas`
+-- Indices de la tabla `recetas`
 --
 ALTER TABLE `recetas`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `recetas_calificacion`
+-- Indices de la tabla `recetas_calificacion`
 --
 ALTER TABLE `recetas_calificacion`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `baules`
+-- AUTO_INCREMENT de la tabla `baules`
 --
 ALTER TABLE `baules`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT for table `lugares`
+-- AUTO_INCREMENT de la tabla `lugares`
 --
 ALTER TABLE `lugares`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
--- AUTO_INCREMENT for table `perfiles`
+-- AUTO_INCREMENT de la tabla `perfiles`
 --
 ALTER TABLE `perfiles`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `recetas`
+-- AUTO_INCREMENT de la tabla `recetas`
 --
 ALTER TABLE `recetas`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
--- AUTO_INCREMENT for table `recetas_calificacion`
+-- AUTO_INCREMENT de la tabla `recetas_calificacion`
 --
 ALTER TABLE `recetas_calificacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
